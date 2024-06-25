a7bcf58572d3ecc6e1d6c32acfd53c7a8b13a753
snwdaaa <158175951+3xtreamhamster@users.noreply.github.com>
2024-06-25 20:02:49 +0900

    Init
    1.0.0

---
+++ src/mod.ts
@@ -0,0 +1,88 @@
+import { DependencyContainer } from "tsyringe";
+import { Ilogger } from "@spt-aki/models/spt/utils/Ilogger";
+import { IPostDBLoadMod } from "@spt-aki/models/external/IPostDBLoadMod";
+import { DatabaseServer } from "@spt-aki/servers/DatabaseServer";
+
+import { VFS } from "@spt-aki/utils/VFS";
+import { jsonc } from "jsonc";
+import path from "path";
+
+class MAPE implements IPostDBLoadMod
+{
+	public mod: string;
+    public modShortName: string;
+
+	constructor() {
+        this.mod = "Make Armor Plate Effective";
+        this.modShortName = "MAPE";
+	}
+
+	public postDBLoad(container: DependencyContainer): void 
+	{
+		const logger = container.resolve<Ilogger>("WinstonLogger");
+		const db = container.resolve<DatabaseServer>("DatabaseServer");
+		const tables = db.getTables();    
+		const itemDB = tables.templates.items;
+
+		const vfs = container.resolve<VFS>("VFS");
+		const config = jsonc.parse(vfs.readFile(path.resolve(__dirname, "../config/config.jsonc")));
+		
+		for (let item in itemDB) {
+			if (itemDB[item]._type !== "Node") {
+				const itemId = itemDB[item]._id
+				if (config.upperTorso.includes(itemId)) {	// if item is in upperTorso table
+					if (config.debug) {
+						logger.info(`[${this.modShortName}] adjusting item ${itemDB[item]._name} (id ${itemId} ) to match config values (front)`);
+					}
+					itemDB[item]._props.Slots[0]._props.filters[0].armorColliders = config.upperTorsoFrontProtectionArea; // set front plate armor collider
+					itemDB[item]._props.Slots[1]._props.filters[0].armorColliders = config.upperTorsoBackProtectionArea; // set back plate armor collider
+					itemDB[item]._props.Slots[0]._props.filters[0].armorPlateColliders = [];
+					itemDB[item]._props.Slots[1]._props.filters[0].armorPlateColliders = [];
+					
+				}
+				else if (config.lowerTorso.includes(itemId)) { // if item is in lowerTorso table
+					if (config.debug) {
+						logger.info(`[${this.modShortName}] adjusting item ${itemDB[item]._name} (id ${itemId} ) to match config values (back)`);
+					}
+					itemDB[item]._props.Slots[0]._props.filters[0].armorColliders = config.lowerTorsoFrontProtectionArea; // set front plate armor collider
+					itemDB[item]._props.Slots[1]._props.filters[0].armorColliders = config.lowerTorsoBackProtectionArea; // set back plate armor collider
+					itemDB[item]._props.Slots[0]._props.filters[0].armorPlateColliders = [];
+					itemDB[item]._props.Slots[1]._props.filters[0].armorPlateColliders = [];
+				}
+				else if (config.entireTorso.includes(itemId)) { // if item is in entireTorso table
+					if (config.debug) {
+						logger.info(`[${this.modShortName}] adjusting item ${itemDB[item]._name} (id ${itemId} ) to match config values (entire)`);
+					}
+					itemDB[item]._props.Slots[0]._props.filters[0].armorColliders = config.entireTorsoFrontProtectionArea; // set front plate armor collider
+					itemDB[item]._props.Slots[1]._props.filters[0].armorColliders = config.entireTorsoBackProtectionArea; // set back plate armor collider
+					itemDB[item]._props.Slots[0]._props.filters[0].armorPlateColliders = [];
+					itemDB[item]._props.Slots[1]._props.filters[0].armorPlateColliders = [];
+				}
+				else if (config.armorPlates.includes(itemId)) { // if item is an armor plate
+					if (config.debug) {
+						logger.info(`[${this.modShortName}] adjusting item ${itemDB[item]._name} (id ${itemId} ) to match config values (plate)`);
+					}
+					itemDB[item]._props.armorColliders = config.armorPlateProtectableArea;
+					itemDB[item]._props.armorPlateColliders = [];
+				}
+				else if (config.frontArmorPlates.includes(itemId)) { // if item is an front armor plate
+					if (config.debug) {
+						logger.info(`[${this.modShortName}] adjusting item ${itemDB[item]._name} (id ${itemId} ) to match config values (front plate)`);
+					}
+					itemDB[item]._props.armorColliders = config.frontArmorPlateProtectableArea;
+					itemDB[item]._props.armorPlateColliders = [];
+				}
+				else if (config.backArmorPlates.includes(itemId)) { // if item is an back armor plate
+					if (config.debug) {
+						logger.info(`[${this.modShortName}] adjusting item ${itemDB[item]._name} (id ${itemId} ) to match config values (back plate)`);
+					}
+					itemDB[item]._props.armorColliders = config.backArmorPlateProtectableArea;
+					itemDB[item]._props.armorPlateColliders = [];
+				}
+			}
+		}
+		logger.info(`[${this.modShortName}] ${this.mod} Loaded`);
+	}
+}
+
+module.exports = { mod: new MAPE() }
